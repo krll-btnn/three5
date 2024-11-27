fd053bf33697edc64006fb63bf0ce5dd5524d870
Кирилл Ботатин (ноут) <botanin.k@mail.ru>
2024-11-27 20:34:45 +0500

    style: Update title and improve code readability
    - Updated the title in index.html for a more accurate representation of the page content.
    - Made minor adjustments in main.js, including formatting and comments, to enhance code readability.

--- main.js
+++ main.js
@@ -2,27 +2,24 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-// Отрисовщик
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.outputColorSpace = THREE.SRGBColorSpace;
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x000000);
-renderer.setPixelRatio(window.devicePixelRatio);
-
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-
-document.body.appendChild(renderer.domElement);
-
 // Сцена
 const scene = new THREE.Scene();
 
+// Отрисовщик
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.outputColorSpace = THREE.SRGBColorSpace;
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x000000);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+document.body.appendChild(renderer.domElement);
+
 // Камера
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-camera.position.set(4, 5, 11);
+camera.position.set(10, 5, 10);
 
-// Перемещение
+// OrbitControls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.enablePan = false;
@@ -34,8 +31,15 @@ controls.autoRotate = false;
 controls.target = new THREE.Vector3(0, 1, 0);
 controls.update();
 
+// Освещение
+const spotLight = new THREE.SpotLight(0xffffff, 3000, 100, 0.25, 1);
+spotLight.position.set(0, 25, 0);
+spotLight.castShadow = true;
+spotLight.shadow.bias = -0.0001;
+scene.add(spotLight);
+
 // Пол
-const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
+const groundGeometry = new THREE.PlaneGeometry(20, 20, 30, 30);
 groundGeometry.rotateX(-Math.PI / 2);
 const groundMaterial = new THREE.MeshStandardMaterial({
   color: 0x555555,
@@ -46,20 +50,13 @@ groundMesh.castShadow = false;
 groundMesh.receiveShadow = true;
 scene.add(groundMesh);
 
-// Свет
-const spotLight = new THREE.SpotLight(0xffffff, 3000, 100, 0.22, 1);
-spotLight.position.set(0, 25, 0);
-spotLight.castShadow = true;
-spotLight.shadow.bias = -0.0001;
-scene.add(spotLight);
-
 // Фрирен
-let frierenModel = null; // Переменная для хранения ссылки на модель
+let frierenModel = null;
 
 const loader = new GLTFLoader().setPath('frieren/');
 loader.load('scene.gltf', (gltf) => {
   console.log('loading model');
-  frierenModel = gltf.scene; // Сохраняем ссылку на модель
+  frierenModel = gltf.scene;
 
   frierenModel.traverse((child) => {
     if (child.isMesh) {
@@ -77,7 +74,7 @@ loader.load('scene.gltf', (gltf) => {
 function animate() {
   requestAnimationFrame(animate);
 
-  frierenModel.rotation.y += 0.01; // Измените значение для настройки скорости
+  frierenModel.rotation.y += 0.02;
 
   controls.update();
   renderer.render(scene, camera);
